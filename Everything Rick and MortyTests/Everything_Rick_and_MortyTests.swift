347b3d7e939ef0f6a6d9f1578f6c0350fc4c6f25
Gareth <garethdharte@gmail.com>
2018-05-06 16:24:30 +0100

    Initial Commit

---
+++ Everything Rick and MortyTests/Everything_Rick_and_MortyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Everything_Rick_and_MortyTests.swift
+//  Everything Rick and MortyTests
+//
+//  Created by Gareth Harte on 06/05/2018.
+//  Copyright © 2018 gharte. All rights reserved.
+//
+
+import XCTest
+@testable import Everything_Rick_and_Morty
+
+class Everything_Rick_and_MortyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
